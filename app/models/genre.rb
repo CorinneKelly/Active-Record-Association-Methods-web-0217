c0583b5625869049d7a5c8df606623116ded7e86
CorinneKelly <corinnemariekelly@gmail.com>
2017-02-15 23:02:03 -0500

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,22 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    self.songs.all.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    self.artists.all.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    array = self.artists
+    name_array = []
+    array.each do |artist|
+      name_array << artist.name
+    end
+
+    name_array
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,9 +3,16 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
+  	self.genre.name
   end
 
   def drake_made_this
     # when this method is called it should assign the song's artist to Drake
+  	# artists = Artists.all
+
+  	drake = Artist.new(name: "Drake")
+ 
+  	self.artist = drake
+  	# pretty sure this is wrong cus it creates a new drake everytime its called
   end
 end
